e14d18c5ce2dc3f8bcc08cdbb88be31fc0a0bf1d
kimjinsoo <roorooro9933@gmail.com>
2020-12-25 14:15:12 +0900

    simulate multiple fetch situation

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
 console.log("start");
 
 function loginUser(email, password, callback) {
-  // 시간을 걸리게 하기 위해서, 즉 async 처럼 동작하도록
-  // 하기 위한 trick이라고 생각하면 된다 setTimeout 이라는 것은.
+  // return을 하는 것이 아니라, 원하는 process가 끝날떄
+  // 새로운 function을 call하고 있다. 이러한 것을 callback function이라고
+  // 이해를 하면 되겠음. 즉 해결 방법을 callback에서 찾고 있다는 것임.
+
   setTimeout(() => {
     console.log("Now we have a data");
     callback({ userEmail: email });
   }, 1500);
 }
 
-// 이렇게 하면은 1.5초가 지나지 않아서 user는 return을 받지 못한다는 것임.
-// 즉 시간이 걸리는 일을 sync 처럼 동작하게 하면은 원하는 결과를 얻지 못한다는 것임.
+function getUserVideos(email, callback) {
+  setTimeout(() => {
+    callback(["video1", "video2", "video3"]);
+  }, 1000);
+}
+
 const user = loginUser("example@google.com", "test", (user) => {
   console.log(user);
+  getUserVideos(user.userEmail, (videos) => {
+    console.log(videos);
+  });
 });
 
 console.log("end");
